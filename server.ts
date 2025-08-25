d7748ebd6d12864b86e93dab2ca0a6781e5e725a
Neil Song <song.neil.song@gmail.com>
2025-08-25 02:10:46 -0700

    fix: types

--- server.ts
+++ server.ts
@@ -3,14 +3,17 @@ import { glob } from "glob";
 const distFiles = await glob("./dist/*.html");
 const modules = await Promise.all(distFiles.map((file) => import("./" + file)));
 
-const routes = distFiles.reduce((acc, file, index) => {
-  const path = file
-    .replace("dist/", "/")
-    .replace(/\.html$/, "")
-    .replace(/\/index$/, "/");
-  acc[path] = modules[index].default;
-  return acc;
-}, {});
+const routes = distFiles.reduce(
+  (acc, file, index) => {
+    const path = file
+      .replace("dist/", "/")
+      .replace(/\.html$/, "")
+      .replace(/\/index$/, "/");
+    acc[path] = modules[index].default;
+    return acc;
+  },
+  {} as Record<string, any>
+);
 
 Bun.serve({
   routes,
